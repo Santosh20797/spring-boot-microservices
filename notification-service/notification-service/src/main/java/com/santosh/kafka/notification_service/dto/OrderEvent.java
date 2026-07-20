6ed9efae7e5a433212f37cf953d3f98fc60c0baf
Santosh Kumar Pal <santoshsiet91@gmail.com>
2026-07-20 16:41:39 +0530

    Added Dockerfiles and updated microservices configuration

--- notification-service/notification-service/src/main/java/com/santosh/kafka/notification_service/dto/OrderEvent.java
+++ notification-service/notification-service/src/main/java/com/santosh/kafka/notification_service/dto/OrderEvent.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class OrderEvent {
 
     private Long orderId;
@@ -14,4 +12,44 @@ public class OrderEvent {
     private Integer quantity;
     private Double price;
     private String status;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
